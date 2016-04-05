741b348f6ac2fd52f69bfc5673109c60655987e0
Sean Segal <Sean_Segal@brown.edu>
2016-04-05 15:34:03 -0400

    Java setup

---
+++ src/main/java/edu/brown/cs/catan/Main.java
@@ -0,0 +1,23 @@
+package edu.brown.cs.catan;
+
+public final class Main {
+  private String[] args;
+
+  /**
+   * Entry point for the application.
+   *
+   * @param args
+   *          The arguments passed to the application.
+   */
+  public static void main(String[] args) {
+    new Main(args).run();
+  }
+
+  private Main(String[] args) {
+    this.args = args;
+  }
+
+  private void run() {
+    // TODO: program runs here
+  }
+}
