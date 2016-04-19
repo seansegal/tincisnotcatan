ab0709db3e2d0c09f2a06a8c605ea6a6b1dbccb6
Sean Segal <Sean_Segal@brown.edu>
2016-04-19 19:01:23 -0400

    Added CatanAPI JSON skeleton

---
+++ src/main/java/edu/brown/cs/catan/CatanAPI.java
@@ -0,0 +1,27 @@
+package edu.brown.cs.catan;
+
+public class CatanAPI {
+
+  private Referee _referee;
+
+  public CatanAPI(){
+    // How are we setting up the players here?
+    _referee = new MasterReferee();
+  }
+
+  public String getBoard(){
+    //TODO
+    return null;
+  }
+
+  public String getHand(){
+    //TODO
+    return null;
+  }
+
+  public String getPlayers(){
+    //TODO
+    return null;
+  }
+
+}
