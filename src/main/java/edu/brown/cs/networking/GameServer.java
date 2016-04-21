c0c8f2ba0b6b31ceb3b2de2c8f9732234d0e7243
Nick DeMarco <nicholas_demarco@brown.edu>
2016-04-20 22:34:05 -0400

    tabs to spaces

--- src/main/java/edu/brown/cs/networking/GameServer.java
+++ src/main/java/edu/brown/cs/networking/GameServer.java
@@ -22,130 +22,142 @@ import spark.template.freemarker.FreeMarkerEngine;
 // arbitrary server class for N users that have to register, etc.
 final class GameServer {
 
-	// use the singleton pattern! This server should only ever have
-	// one connection to the web / queries, etc.
-	private static GameServer sharedInstance = new GameServer();
+  // use the singleton pattern! This server should only ever have
+  // one connection to the web / queries, etc.
+  private static GameServer    sharedInstance = new GameServer();
 
-	// all of the Sessions that are connected to this server.
-	private Map<Session, String> userIds = new HashMap<>();
+  // all of the Sessions that are connected to this server.
+  private Map<Session, String> userIds        = new HashMap<>();
 
-	// a simple counter for userIDs - to remove?
-	private int nextUserID = 1;
+  // a simple counter for userIDs - to remove?
+  private int                  nextUserID     = 1;
 
-	// initialize the game server
-	public static GameServer getInstance() {
-		return sharedInstance;
-	}
 
-	private GameServer() {
-	}
+  // initialize the game server
+  public static GameServer getInstance() {
+    return sharedInstance;
+  }
 
-	public void launch() {
-		try {
-			Spark.externalStaticFileLocation("src/main/resources/static");
-			Spark.port(getHerokuAssignedPort());
 
-			Configuration config = new Configuration();
-			File templates = new File(
-					"src/main/resources/spark/template/freemarker");
-			config.setDirectoryForTemplateLoading(templates);
-			FreeMarkerEngine freeMarker = new FreeMarkerEngine(config);
+  private GameServer() {}
 
-			// set up chat
-			Spark.webSocket("/chat", ChatWebSocketHandler.class);
-			Spark.webSocket("/action", ActionWebSocketHandler.class);
 
-			// Set up board
-			Spark.get("/board", new BoardHandler(), freeMarker);
+  public void launch() {
+    try {
+      Spark.externalStaticFileLocation("src/main/resources/static");
+      Spark.port(getHerokuAssignedPort());
 
-			// secure("", "", "", ""); // use this for https!
-			Spark.init();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+      Configuration config = new Configuration();
+      File templates = new File(
+          "src/main/resources/spark/template/freemarker");
+      config.setDirectoryForTemplateLoading(templates);
+      FreeMarkerEngine freeMarker = new FreeMarkerEngine(config);
 
-	// Sends a message from one user to all users, along with a list of current
-	// usernames
-	public void broadcastMessage(String sender, String message) {
+      // set up chat
+      Spark.webSocket("/chat", ChatWebSocketHandler.class);
+      Spark.webSocket("/action", ActionWebSocketHandler.class);
 
-		// a session is a single user interacting with the server.
-		for (Session session : userIds.keySet()) {
-			if (session.isOpen()) { // if the session is still active.
+      // Set up board
+      Spark.get("/board", new BoardHandler(), freeMarker);
 
-				// Return a reference to the RemoteEndpoint object representing
-				// the
-				// other end of this conversation.
-				RemoteEndpoint remEnd = session.getRemote();
+      // secure("", "", "", ""); // use this for https!
+      Spark.init();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-				// create the JSONObject we want to send.
-				// this could be anything!
-				JSONObject toSend = Chat.createMessage(sender, message,
-						userIds.values());
 
-				// send the string to the session
-				try {
-					remEnd.sendString(String.valueOf(toSend));
-				} catch (IOException e) {
-					System.out
-							.println("IOException while sending JSON to session.");
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+  // Sends a message from one user to all users, along with a list of current
+  // usernames
+  public void broadcastMessage(String sender, String message) {
 
-	/**
-	 * Indicate if a given session is in our chat.
-	 *
-	 * @param user
-	 *            the Session object
-	 * @return true if this {@code user} is in the chat.
-	 */
-	public boolean contains(Session user) {
-		return userIds.containsKey(user);
-	}
+    // a session is a single user interacting with the server.
+    for (Session session : userIds.keySet()) {
+      if (session.isOpen()) { // if the session is still active.
 
-	public int activeUsers() {
-		return userIds.keySet().size();
-	}
+        // Return a reference to the RemoteEndpoint object representing
+        // the
+        // other end of this conversation.
+        RemoteEndpoint remEnd = session.getRemote();
 
-	public int getAndIncrementId() {
-		return nextUserID++;
-	}
+        // create the JSONObject we want to send.
+        // this could be anything!
+        JSONObject toSend = Chat.createMessage(sender, message,
+            userIds.values());
 
-	public boolean remove(Session toRemove) {
-		return userIds.remove(toRemove) != null;
-	}
+        // send the string to the session
+        try {
+          remEnd.sendString(String.valueOf(toSend));
+        } catch (IOException e) {
+          System.out
+              .println("IOException while sending JSON to session.");
+          e.printStackTrace();
+        }
+      }
+    }
+  }
 
-	public boolean add(Session toAdd, String id) {
-		return userIds.put(toAdd, id) != id;
-	}
 
-	public String get(Session toGet) {
-		return userIds.get(toGet);
-	}
+  /**
+   * Indicate if a given session is in our chat.
+   *
+   * @param user
+   *          the Session object
+   * @return true if this {@code user} is in the chat.
+   */
+  public boolean contains(Session user) {
+    return userIds.containsKey(user);
+  }
 
-	// used for heroku hosting - environment variables are set by heroku.
-	private int getHerokuAssignedPort() {
-		ProcessBuilder processBuilder = new ProcessBuilder();
-		if (processBuilder.environment().get("PORT") != null) {
-			return Integer.parseInt(processBuilder.environment().get("PORT"));
-		}
-		return 4567; // return default port if heroku-port isn't set (i.e. on
-						// localhost)
-	}
 
-	/**
-	 * A class which controls the initial page for maps.
-	 */
-	private static class BoardHandler implements TemplateViewRoute {
-		@Override
-		public ModelAndView handle(Request req, Response res) {
-			Map<String, Object> variables = ImmutableMap.of("title",
-					"Play Catan");
-			return new ModelAndView(variables, "board.ftl");
-		}
-	}
+  public int activeUsers() {
+    return userIds.keySet().size();
+  }
+
+
+  public int getAndIncrementId() {
+    return nextUserID++;
+  }
+
+
+  public boolean remove(Session toRemove) {
+    return userIds.remove(toRemove) != null;
+  }
+
+
+  public boolean add(Session toAdd, String id) {
+    return userIds.put(toAdd, id) != id;
+  }
+
+
+  public String get(Session toGet) {
+    return userIds.get(toGet);
+  }
+
+
+  // used for heroku hosting - environment variables are set by heroku.
+  private int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+      return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    return 4567; // return default port if heroku-port isn't set (i.e. on
+    // localhost)
+  }
+
+
+  /**
+   * A class which controls the initial page for maps.
+   */
+  private static class BoardHandler implements TemplateViewRoute {
+
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      Map<String, Object> variables = ImmutableMap.of("title",
+          "Play Catan");
+      return new ModelAndView(variables, "board.ftl");
+    }
+  }
 
 }
