022df730b0ba545971aa22c06034bd7430a6c52b
Nick DeMarco <nicholas_demarco@brown.edu>
2016-04-21 01:11:35 -0400

    json messages now labeled with query type

--- src/main/java/edu/brown/cs/networking/ActionWebSocketHandler.java
+++ src/main/java/edu/brown/cs/networking/ActionWebSocketHandler.java
@@ -1,7 +1,6 @@
 package edu.brown.cs.networking;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -11,33 +10,36 @@ import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.google.gson.Gson;
 
 @WebSocket
 public class ActionWebSocketHandler {
 
-  private final GameServer gs = GameServer.getInstance();
+  private final GameServer          gs          = GameServer.getInstance();
   private static final Set<Session> activeUsers = new HashSet<>();
 
-  private static final int NUM_PLAYERS = 4;
+  private static final int          NUM_PLAYERS = 4;
 
-  private static final Gson GSON = new Gson();
+  private static final Gson         GSON        = new Gson();
 
 
   @OnWebSocketConnect
   synchronized public void onConnect(Session user) throws Exception {
     System.out.println("Connected " + user.getLocalAddress());
-    if(activeUsers.size() < NUM_PLAYERS) {
+    if (activeUsers.size() < NUM_PLAYERS) {
       activeUsers.add(user);
     }
   }
 
 
   @OnWebSocketClose
-  synchronized public void onClose(Session user, int statusCode, String reason) {
+  synchronized public void onClose(Session user, int statusCode,
+      String reason) {
     System.out.println("Disconnected from " + user.getLocalAddress());
-    if(activeUsers.remove(user)){
+    if (activeUsers.remove(user)) {
       System.out.println("PLAYER CLOSED: " + user.getLocalAddress());
     } else {
       System.out.println("WITNESS CLOSED: " + user.getLocalAddress());
@@ -48,18 +50,32 @@ public class ActionWebSocketHandler {
   @OnWebSocketMessage
   synchronized public void onMessage(Session user, String message) {
 
-    if(activeUsers.contains(user)) {
+    if (activeUsers.contains(user)) {
       System.out.println("Received message from " + user.getLocalAddress());
       System.out.println("Message : " + message);
+      Map<String, String> map = GSON.fromJson(message, Map.class);
+      String returnMessage = "";
+      if(!map.containsKey("action")){
+        returnMessage = "Unsupported command : " + map.toString();
+      } else {
 
+        Object toRet = GSON.fromJson(gs.handleAction(message), Map.class);
+        JSONObject j = new JSONObject();
+        try {
+          j.put(map.get("action"), toRet);
+          returnMessage = j.toString();
+        } catch (JSONException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
       try {
-        Map<String, String> resp = new HashMap<>();
-        resp.put(message, gs.handleAction(message));
-        user.getRemote().sendString(GSON.toJson(resp));
+        user.getRemote().sendString(returnMessage);
       } catch (IOException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
       }
+
       // do something with that action.
     } else {
       System.out.println("Received invalid message from non-player");
--- src/main/java/edu/brown/cs/networking/CatanServer.java
+++ src/main/java/edu/brown/cs/networking/CatanServer.java
@@ -1,19 +1,24 @@
 package edu.brown.cs.networking; // eventually move into catan
 
+import com.google.gson.Gson;
+
 import edu.brown.cs.api.CatanAPI;
 
 // Catan-specific logic for game
 // holds reference to GameServer
 public final class CatanServer {
 
+  private static final Gson gson = new Gson();
+
+
   public CatanServer(CatanAPI api) {
     GameServer.getInstance().launch((req) -> {
-      switch(req) {
-        case "getBoard" :
+      switch (req) {
+        case "getBoard":
           return api.getBoard();
-        case "getPlayers" :
+        case "getPlayers":
           return api.getPlayers();
-        default :
+        default:
           throw new IllegalArgumentException("Unsupported action : " + req);
       }
     });
