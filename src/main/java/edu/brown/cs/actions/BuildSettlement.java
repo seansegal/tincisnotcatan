c3ce3131ff53f9b42e68416975a24886b34cf512
Sean Segal <Sean_Segal@brown.edu>
2016-04-22 14:05:01 -0400

    Added cities and roads to test board

--- src/main/java/edu/brown/cs/actions/BuildSettlement.java
+++ src/main/java/edu/brown/cs/actions/BuildSettlement.java
@@ -2,6 +2,8 @@ package edu.brown.cs.actions;
 
 import java.util.Map;
 
+import com.google.common.collect.ImmutableMap;
+
 import edu.brown.cs.board.Intersection;
 import edu.brown.cs.board.IntersectionCoordinate;
 import edu.brown.cs.catan.Player;
@@ -16,31 +18,42 @@ public class BuildSettlement implements Action {
   private boolean _mustPay;
   private Referee _ref;
 
-  public BuildSettlement(Referee ref, int playerID, IntersectionCoordinate i, boolean mustPay) {
+  public BuildSettlement(Referee ref, int playerID, IntersectionCoordinate i,
+      boolean mustPay) {
+    assert ref != null && i != null;
     _ref = ref;
-    _player = ref.getPlayerByID(playerID);
-    _intersection = ref.getBoard().getIntersections().get(i);
+    _player = _ref.getPlayerByID(playerID);
+    _intersection = _ref.getBoard().getIntersections().get(i);
     _mustPay = mustPay;
+    if (_player == null) {
+      String err = String.format("No player exists with the id: %d", playerID);
+      throw new IllegalArgumentException(err);
+    }
+    if (_intersection == null) {
+      throw new IllegalArgumentException("The intersection could not be found.");
+    }
+
   }
 
   @Override
   public Map<Integer, ActionResponse> execute() {
-    if(_mustPay) {
+    if (_mustPay) {
       Map<Resource, Double> resources = _player.getResources();
-     Settings.SETTLEMENT_COST.forEach((res, amount) -> {
-       if(resources.get(res) < amount){
-         //TODO: throw error
-       }
-     });
-     _player.buildSettlement();
+      for (Map.Entry<Resource, Double> cost : Settings.SETTLEMENT_COST
+          .entrySet()) {
+        if (resources.get(cost.getKey()) < cost.getValue()) {
+          return ImmutableMap.of(_player.getID(), new ActionResponse(false,
+              "You do not have the resources to afford a Settlement.", null));
+        }
+      }
+      _player.buildSettlement();
     }
-    //TODO: add turn validation, add graph validation
+    // TODO: add turn validation, add graph validation
     _player.useSettlement();
     _intersection.placeSettlement(_player);
 
+    // TODO: formulate response & send.
     return null;
   }
-
-
 
 }
