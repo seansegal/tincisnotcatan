0d72f849cc6bfb8ab6f20fe240d332d5eb6f1617
Sean Segal <Sean_Segal@brown.edu>
2016-04-23 13:19:02 -0400

    Added TODO comment for GraphCache

--- src/main/java/edu/brown/cs/board/GraphCache.java
+++ src/main/java/edu/brown/cs/board/GraphCache.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class GraphCache {
+
+  // TODO: fix so that we can support multiple games.
   public final static Map<Intersection, CatanNode> nodes = new HashMap<Intersection, CatanNode>();
   public final static Map<Path, CatanEdge> edges = new HashMap<Path, CatanEdge>();
 }
