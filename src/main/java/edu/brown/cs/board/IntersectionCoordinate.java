000785ce6727423811c6bcb3566448221b06bd3c
Sean Segal <Sean_Segal@brown.edu>
2016-04-23 12:41:11 -0400

    Remvoed underscore from IntersectionCoordinate

--- src/main/java/edu/brown/cs/board/IntersectionCoordinate.java
+++ src/main/java/edu/brown/cs/board/IntersectionCoordinate.java
@@ -1,35 +1,35 @@
 package edu.brown.cs.board;
 
 public class IntersectionCoordinate {
-  private final HexCoordinate _coord1;
-  private final HexCoordinate _coord2;
-  private final HexCoordinate _coord3;
+  private final HexCoordinate coord1;
+  private final HexCoordinate coord2;
+  private final HexCoordinate coord3;
 
   public IntersectionCoordinate(HexCoordinate coord1, HexCoordinate coord2,
       HexCoordinate coord3) {
-    _coord1 = coord1;
-    _coord2 = coord2;
-    _coord3 = coord3;
+    this.coord1 = coord1;
+    this.coord2 = coord2;
+    this.coord3 = coord3;
   }
 
   public HexCoordinate getCoord1() {
-    return _coord1;
+    return coord1;
   }
 
   public HexCoordinate getCoord2() {
-    return _coord2;
+    return coord2;
   }
 
   public HexCoordinate getCoord3() {
-    return _coord3;
+    return coord3;
   }
 
   @Override
   public int hashCode() {
     int result;
-    result = ((_coord1 == null) ? 0 : _coord1.hashCode());
-    result += ((_coord2 == null) ? 0 : _coord2.hashCode());
-    result += ((_coord3 == null) ? 0 : _coord3.hashCode());
+    result = ((coord1 == null) ? 0 : coord1.hashCode());
+    result += ((coord2 == null) ? 0 : coord2.hashCode());
+    result += ((coord3 == null) ? 0 : coord3.hashCode());
     return result;
   }
 
@@ -39,34 +39,34 @@ public class IntersectionCoordinate {
       return false;
     }
     IntersectionCoordinate toComp = (IntersectionCoordinate) obj;
-    if (_coord1.equals(toComp.getCoord1())
-        && _coord2.equals(toComp.getCoord2())
-        && _coord3.equals(toComp.getCoord3())) {
+    if (coord1.equals(toComp.getCoord1())
+        && coord2.equals(toComp.getCoord2())
+        && coord3.equals(toComp.getCoord3())) {
       return true;
     }
-    if (_coord1.equals(toComp.getCoord1())
-        && _coord2.equals(toComp.getCoord3())
-        && _coord3.equals(toComp.getCoord2())) {
+    if (coord1.equals(toComp.getCoord1())
+        && coord2.equals(toComp.getCoord3())
+        && coord3.equals(toComp.getCoord2())) {
       return true;
     }
-    if (_coord1.equals(toComp.getCoord2())
-        && _coord2.equals(toComp.getCoord1())
-        && _coord3.equals(toComp.getCoord3())) {
+    if (coord1.equals(toComp.getCoord2())
+        && coord2.equals(toComp.getCoord1())
+        && coord3.equals(toComp.getCoord3())) {
       return true;
     }
-    if (_coord1.equals(toComp.getCoord3())
-        && _coord2.equals(toComp.getCoord2())
-        && _coord3.equals(toComp.getCoord1())) {
+    if (coord1.equals(toComp.getCoord3())
+        && coord2.equals(toComp.getCoord2())
+        && coord3.equals(toComp.getCoord1())) {
       return true;
     }
-    if (_coord1.equals(toComp.getCoord3())
-        && _coord2.equals(toComp.getCoord1())
-        && _coord3.equals(toComp.getCoord2())) {
+    if (coord1.equals(toComp.getCoord3())
+        && coord2.equals(toComp.getCoord1())
+        && coord3.equals(toComp.getCoord2())) {
       return true;
     }
-    if (_coord1.equals(toComp.getCoord2())
-        && _coord2.equals(toComp.getCoord3())
-        && _coord3.equals(toComp.getCoord1())) {
+    if (coord1.equals(toComp.getCoord2())
+        && coord2.equals(toComp.getCoord3())
+        && coord3.equals(toComp.getCoord1())) {
       return true;
     }
 
@@ -76,11 +76,11 @@ public class IntersectionCoordinate {
   @Override
   public String toString() {
     StringBuilder toRet = new StringBuilder();
-    toRet.append(_coord1.toString());
+    toRet.append(coord1.toString());
     toRet.append(" ");
-    toRet.append(_coord2.toString());
+    toRet.append(coord2.toString());
     toRet.append(" ");
-    toRet.append(_coord3.toString());
+    toRet.append(coord3.toString());
     toRet.append("\n");
     return toRet.toString();
   }
