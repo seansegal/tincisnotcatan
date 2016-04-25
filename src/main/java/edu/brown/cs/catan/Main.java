10b8c361080300fec05b188a8f07c0d98fcc98a7
Nick DeMarco <nicholas_demarco@brown.edu>
2016-04-25 11:13:58 -0400

    updated main and pom, check for heroku env in main to set file paths

--- src/main/java/edu/brown/cs/catan/Main.java
+++ src/main/java/edu/brown/cs/catan/Main.java
@@ -17,13 +17,22 @@ import spark.template.freemarker.FreeMarkerEngine;
 
 public class Main {
 
-  private static final int NUM_THREADS = 8;
-  private static final int MIN_THREADS = 2;
-  private static final int TIMEOUT = 3600000;
+  private static final int    NUM_THREADS      = 8;
+  private static final int    MIN_THREADS      = 2;
+  private static final int    TIMEOUT          = 3600000;
+
+  private static final String STATIC_FILE_PATH =
+      System.getenv("HEROKU") != null ? "target/classes/static"
+          : "src/main/resources/static";
+
+  private static final String TEMPLATE_PATH    =
+      System.getenv("HEROKU") != null
+          ? "target/classes/spark/template/freermarker"
+          : "src/main/resources/spark/template/freemarker";
 
 
   public static void main(String[] args) {
-    Spark.externalStaticFileLocation("src/main/resources/static");
+    Spark.externalStaticFileLocation(STATIC_FILE_PATH);
     Spark.port(getHerokuAssignedPort());
     Spark.threadPool(NUM_THREADS, MIN_THREADS, TIMEOUT);
     // secure("", "", "", ""); // use this for https!
