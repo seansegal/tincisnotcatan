4759b952833cb56f0736a65736f1f1f31081f47b
Sean Segal <Sean_Segal@brown.edu>
2016-04-27 12:37:42 -0400

    Rolling 7 works

--- src/main/java/edu/brown/cs/actions/RollDice.java
+++ src/main/java/edu/brown/cs/actions/RollDice.java
@@ -120,13 +120,14 @@ public class RollDice implements Action {
       }
     } else {
       // 7 is rolled:
+      System.out.println("7 WAS ROLLED!!!!!!!!");
       boolean mustDiscard = false;
       String message = "7 was rolled.";
       for (Player p : _ref.getPlayers()) {
         if (p.getNumResourceCards() > Settings.DROP_CARDS_THRESH) {
           mustDiscard = true;
           _ref.playerMustDiscard(p.getID());
-          message += String.format(" %s must discard cards");
+          message += String.format(" %s must discard cards", p.getName());
         }
       }
       if (mustDiscard) {
@@ -134,7 +135,8 @@ public class RollDice implements Action {
         for (Player p : _ref.getPlayers()) {
           if (_ref.playerHasDiscarded(p.getID())) {
             toRet.put(p.getID(), new ActionResponse(true,
-                "7 was rolled. You must drop cards.", "dropCards", null));
+                "7 was rolled. You must drop half of your cards.", "dropCards",
+                null));
           } else {
             toRet.put(p.getID(), new ActionResponse(true, message, null));
           }
@@ -153,6 +155,7 @@ public class RollDice implements Action {
         }
       }
     }
+    System.out.println(toRet);
     return toRet;
   }
 
