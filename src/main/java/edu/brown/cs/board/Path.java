da4af0bd232eba6719c86dacdb445f9ac80f4e9a
Ansel Vahle <ansel_vahle@brown.edu>
2016-04-27 16:22:14 -0400

    Implemented random road validation

--- src/main/java/edu/brown/cs/board/Path.java
+++ src/main/java/edu/brown/cs/board/Path.java
@@ -52,31 +52,36 @@ public class Path {
     if (_road != null) {
       return false;
     }
-
-    if (_start.getBuilding() != null
-        && _start.getBuilding().getPlayer().equals(p)) {
-      System.out.println("Here Here");
+    if (Math.random() > .5) {
       return true;
-    } else if (_start.getBuilding() == null) {
-      for (Path path : _start.getPaths()) {
-        if (path.getRoad() != null && path.getRoad().getPlayer().equals(p)) {
-          System.out.println("Now Here");
-          return true;
-        }
-      }
-    }
-    if (_end.getBuilding() != null && _end.getBuilding().getPlayer().equals(p)) {
-      System.out.println("Still Here " + p.getID());
-      return true;
-    } else if (_end.getBuilding() == null) {
-      for (Path path : _end.getPaths()) {
-        if (path.getRoad() != null && path.getRoad().getPlayer().equals(p)) {
-          System.out.println("Here");
-          return true;
-        }
-      }
     }
     return false;
+
+    // if (_start.getBuilding() != null
+    // && _start.getBuilding().getPlayer().equals(p)) {
+    // System.out.println("Here Here");
+    // return true;
+    // } else if (_start.getBuilding() == null) {
+    // for (Path path : _start.getPaths()) {
+    // if (path.getRoad() != null && path.getRoad().getPlayer().equals(p)) {
+    // System.out.println("Now Here");
+    // return true;
+    // }
+    // }
+    // }
+    // if (_end.getBuilding() != null &&
+    // _end.getBuilding().getPlayer().equals(p)) {
+    // System.out.println("Still Here " + p.getID());
+    // return true;
+    // } else if (_end.getBuilding() == null) {
+    // for (Path path : _end.getPaths()) {
+    // if (path.getRoad() != null && path.getRoad().getPlayer().equals(p)) {
+    // System.out.println("Here");
+    // return true;
+    // }
+    // }
+    // }
+    // return false;
   }
 
   public void placeRoad(Player p) {
