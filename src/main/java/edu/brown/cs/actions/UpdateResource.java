fe68f5c3c89b051bc546990b8c374d7a6d1acdaf
Ansel Vahle <ansel_vahle@brown.edu>
2016-05-05 16:52:43 -0400

    Implemented Konami Code of backend

---
+++ src/main/java/edu/brown/cs/actions/UpdateResource.java
@@ -0,0 +1,40 @@
+package edu.brown.cs.actions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
+import edu.brown.cs.catan.Player;
+import edu.brown.cs.catan.Referee;
+import edu.brown.cs.catan.Resource;
+
+public class UpdateResource implements Action {
+  private Player _player;
+  private Referee _ref;
+
+  public UpdateResource(Referee ref, int playerID) {
+    assert ref != null;
+    _ref = ref;
+    _player = ref.getPlayerByID(playerID);
+    if (_player == null) {
+      throw new IllegalArgumentException(String.format(
+          "No player exists with ID: %d", playerID));
+    }
+  }
+
+  @Override
+  public Map<Integer, ActionResponse> execute() {
+    Map<Resource, Double> resources = new HashMap<>();
+    for (Resource res : _player.getResources().keySet()) {
+      _player.removeResource(res, _player.getResources().get(res),
+          _ref.getBank());
+      _player.addResource(res, 99.0, _ref.getBank());
+      resources.put(res, 99.0);
+    }
+    String message = "You unlocked the power!";
+    ActionResponse toAdd = new ActionResponse(true, message, resources);
+    return ImmutableMap.of(_player.getID(), toAdd);
+  }
+
+}
--- src/main/java/edu/brown/cs/api/ActionFactory.java
+++ src/main/java/edu/brown/cs/api/ActionFactory.java
@@ -20,6 +20,7 @@ import edu.brown.cs.actions.ProposeTrade;
 import edu.brown.cs.actions.StartGame;
 import edu.brown.cs.actions.StartGameSetup;
 import edu.brown.cs.actions.TradeWithBank;
+import edu.brown.cs.actions.UpdateResource;
 import edu.brown.cs.board.HexCoordinate;
 import edu.brown.cs.board.IntersectionCoordinate;
 import edu.brown.cs.catan.MasterReferee;
@@ -125,6 +126,8 @@ public class ActionFactory {
           return new EndTurn(_referee, playerID);
         case "proposeTrade":
           return new ProposeTrade(_referee, playerID, actionJSON);
+        case "updateResource":
+          return new UpdateResource(_referee, playerID);
         default:
           String err = String.format("The action %s does not exist.", action);
           throw new IllegalArgumentException(err);
