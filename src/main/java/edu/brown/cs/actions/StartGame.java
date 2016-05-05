e8d11496de437b762fd99dc508f818e77a5bbc98
Sean Segal <Sean_Segal@brown.edu>
2016-05-05 16:42:30 -0400

    Changed game start

--- src/main/java/edu/brown/cs/actions/StartGame.java
+++ src/main/java/edu/brown/cs/actions/StartGame.java
@@ -1,9 +1,13 @@
 package edu.brown.cs.actions;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import com.google.common.collect.ImmutableList;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
@@ -37,9 +41,30 @@ public class StartGame implements Action {
           json.add("turnOrder", turnOrder);
           json.addProperty("isFirst",
               firstToGo == player.getID());
-          toReturn.put(player.getID(), new ActionResponse(true, "", json));
+          toReturn.put(player.getID(), new ActionResponse(true, "Let the games begin!", json));
         });
-    _ref.addFollowUp(ImmutableList.of(new StartGameSetup(firstToGo)));
+
+    List<Integer> setupOrder = _ref.getSetup().getSetupOrder();
+    Set<Integer> placedFirstSettlement = new HashSet<>();
+    int i = 0;
+    for (int id : setupOrder) {
+      Collection<FollowUpAction> followUp = new ArrayList<>();
+      if (!placedFirstSettlement.contains(id)) {
+        followUp.add(new PlaceInitialSettlement(id, 1));
+        placedFirstSettlement.add(id);
+      } else {
+        followUp.add(new PlaceInitialSettlement(id, 2));
+      }
+      _ref.addFollowUp(followUp);
+      followUp = new ArrayList<>();
+      if (i == setupOrder.size() - 1) {
+        followUp.add(new PlaceRoad(id, true));
+      } else {
+        followUp.add(new PlaceRoad(id, false));
+      }
+      _ref.addFollowUp(followUp);
+      i++;
+    }
     _ref.setGameStatus(GameStatus.SETUP);
     return toReturn;
   }
