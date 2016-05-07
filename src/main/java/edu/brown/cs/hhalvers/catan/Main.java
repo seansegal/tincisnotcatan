302c56c405b2c05f523c61616a07f7bce9357bfd
Ansel Vahle <ansel_vahle@brown.edu>
2016-05-07 14:47:25 -0400

    Added classes for testing and deleted Hans's catan package

--- src/main/java/edu/brown/cs/hhalvers/catan/Main.java
+++
@@ -1,8 +0,0 @@
-package edu.brown.cs.hhalvers.catan;
-
-public class Main {
-
-	public static void main(String[] args) {
-	}
-
-}
---
+++ src/test/java/edu/brown/cs/catan/TradeTest.java
@@ -0,0 +1,5 @@
+package edu.brown.cs.catan;
+
+public class TradeTest {
+
+}
