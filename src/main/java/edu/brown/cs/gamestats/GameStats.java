29e8bda8c84aff580207f382354f9c244965c2d9
Sean Segal <Sean_Segal@brown.edu>
2016-05-07 17:36:13 -0400

    Sending Rolls and Turn # in gameState

--- src/main/java/edu/brown/cs/gamestats/GameStats.java
+++ src/main/java/edu/brown/cs/gamestats/GameStats.java
@@ -26,7 +26,23 @@ public class GameStats {
     }
   }
 
-  public void usedCheating(){
+  public int[] getRollsArray() {
+    int[] rolls = new int[11];
+    rolls[0] = _rolls.containsKey(2) ? _rolls.get(2) : 0;
+    rolls[1] = _rolls.containsKey(3) ? _rolls.get(3) : 0;
+    rolls[2] = _rolls.containsKey(4) ? _rolls.get(4) : 0;
+    rolls[3] = _rolls.containsKey(5) ? _rolls.get(5) : 0;
+    rolls[4] = _rolls.containsKey(6) ? _rolls.get(6) : 0;
+    rolls[5] = _rolls.containsKey(7) ? _rolls.get(7) : 0;
+    rolls[6] = _rolls.containsKey(8) ? _rolls.get(8) : 0;
+    rolls[7] = _rolls.containsKey(9) ? _rolls.get(9) : 0;
+    rolls[8] = _rolls.containsKey(10) ? _rolls.get(10) : 0;
+    rolls[9] = _rolls.containsKey(11) ? _rolls.get(11) : 0;
+    rolls[10] = _rolls.containsKey(12) ? _rolls.get(12) : 0;
+    return rolls;
+  }
+
+  public void usedCheating() {
     _cheatingUsed = true;
   }
 
