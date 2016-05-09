b19628d19d8a8e40166045888df4ab4941647243
Ansel Vahle <ansel_vahle@brown.edu>
2016-05-09 13:09:30 -0400

    Removed TradeResponse followUp when all canceled

--- src/main/java/edu/brown/cs/actions/ReviewTrade.java
+++ src/main/java/edu/brown/cs/actions/ReviewTrade.java
@@ -75,6 +75,8 @@ public class ReviewTrade implements FollowUpAction {
         String message = "Everyone declined the trade";
         ActionResponse toAdd = new ActionResponse(true, message, response);
         toRet.put(p.getID(), toAdd);
+        _ref.removeFollowUp(new TradeResponse(_trade.getTrader(), _resources,
+            _trade));
       } else if (!p.equals(_player)) {
         String message = String.format("%s declined the trade",
             _player.getName());
