6f0bcf27d85809fbd5012d3b92c9c8a8010da02b
Ansel Vahle <ansel_vahle@brown.edu>
2016-05-14 17:41:34 -0400

    Commented Road

--- src/main/java/edu/brown/cs/board/Road.java
+++ src/main/java/edu/brown/cs/board/Road.java
@@ -2,13 +2,30 @@ package edu.brown.cs.board;
 
 import edu.brown.cs.catan.Player;
 
+/**
+ * Class that is the representation of roads.
+ *
+ * @author anselvahle
+ *
+ */
 public class Road {
   private Player _player;
 
+  /**
+   * Constructor for the class.
+   * 
+   * @param player
+   *          Player who is associated with this road.
+   */
   public Road(Player player) {
     _player = player;
   }
 
+  /**
+   * Gets the player associated with this road.
+   *
+   * @return The player whose road this is.
+   */
   public Player getPlayer() {
     return _player;
   }
