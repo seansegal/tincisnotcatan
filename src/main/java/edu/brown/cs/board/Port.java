dce9f03cc166addd5dd6367073e73f4147a8af6b
Ansel Vahle <ansel_vahle@brown.edu>
2016-05-14 17:35:14 -0400

    Commented Port

--- src/main/java/edu/brown/cs/board/Port.java
+++ src/main/java/edu/brown/cs/board/Port.java
@@ -2,13 +2,30 @@ package edu.brown.cs.board;
 
 import edu.brown.cs.catan.Resource;
 
+/**
+ * Class that is for the representation of ports.
+ *
+ * @author anselvahle
+ *
+ */
 public class Port {
   private Resource _resource;
 
+  /**
+   * Constructor for the class.
+   *
+   * @param resource
+   *          Resource that is the type of the port.
+   */
   public Port(Resource resource) {
     _resource = resource;
   }
 
+  /**
+   * Gets the type of the port.
+   * 
+   * @return Resource associated with the port.
+   */
   public Resource getResource() {
     return _resource;
   }
